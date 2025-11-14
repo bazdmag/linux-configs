c88d72ada95fe2a30b4d4c7e9c25c0784902f5be
bazdmag <sztojkaattila383@gmail.com>
2025-11-14 14:34:07 +0100

    Disable GRUB theme and btrfs installation in install.sh
    Comment out GRUB theme and btrfs installation steps.

--- install.sh
+++ install.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-
-
-
 echo "Starting installion please wait..." 
 
-echo "running GRUB theme installion"
-sudo $(pwd)/darkmatter-grub-theme/darkmatter-theme.py -i
+#echo "running GRUB theme installion"
+#sudo $(pwd)/darkmatter-grub-theme/darkmatter-theme.py -i
 
-echo "Running GRUB btrfsinstallion"
-sudo $(pwd)/.grub-btrfs.sh
+#echo "Running GRUB btrfsinstallion"
+#sudo $(pwd)/.grub-btrfs.sh
 
 #echo "Running user theme installer"
 #$(pwd)/.OS-theme.sh
